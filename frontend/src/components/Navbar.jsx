b0e24011fa8d6ac2c95951dc02568a9c458edbc9
rovid19 <118137968+rovid19@users.noreply.github.com>
2023-03-09 22:50:12 +0100

    try
    try

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,25 +7,34 @@ import {
   Grid,
   Container,
   Item,
+  Input,
 } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import useTheme from "@mui/material";
 
 const Navbar = () => {
   return (
     <>
       <Container>
-        <Grid container spacing={2}>
-          {" "}
-          <Grid item xs={3}>
-            <Typography> Mirko </Typography>
-          </Grid>
-          <Grid item xs={7}>
-            <Typography> Mirko </Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <Typography> Mirko </Typography>
-          </Grid>
-        </Grid>
+        <AppBar>
+          <Toolbar>
+            <Container>
+              <Grid container spacing={2}>
+                {" "}
+                <Grid item xs={3}>
+                  <Typography> E-trade </Typography>
+                </Grid>
+                <Grid item xs={7}>
+                  <Input></Input>
+                  <SearchIcon />
+                </Grid>
+                <Grid item xs={2}>
+                  <Typography> Mirko </Typography>
+                </Grid>
+              </Grid>
+            </Container>
+          </Toolbar>
+        </AppBar>
       </Container>
     </>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,27 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/system";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#FFFFFC",
-    },
-    secondary: {
-      main: "#32292F",
-    },
-    third: {
-      main: "#E15554",
-    },
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </BrowserRouter>
 );
