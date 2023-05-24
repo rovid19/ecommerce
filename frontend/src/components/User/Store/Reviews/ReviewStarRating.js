ebe4c72dd08f7e4902ad00506ecb6b2f59de6d26
rovid19 <118137968+rovid19@users.noreply.github.com>
2023-05-25 00:00:50 +0200

    star rating
    review star rating

--- frontend/src/components/User/Store/Reviews/ReviewStarRating.js
+++ frontend/src/components/User/Store/Reviews/ReviewStarRating.js
@@ -1,44 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
 const ReviewStarRating = () => {
+  const [rating, setRating] = useState(null);
   return (
-    <fieldset className="w-full h-full fl2 swanna">
-      <h1 className="mb-2 text-3xl">Rate this product</h1>
-      <div>
-        <label for="star5">
-          <input id="star5" type="radio" className="rating" value="5" />
-        </label>
-        <label for="star4.5">
-          <input id="star4.5" type="radio" className="rating" value="4.5" />
-        </label>
-        <label for="star4">
-          <input id="star4" type="radio" className="rating" value="4" />
-        </label>
-        <label for="star3.5">
-          <input id="star3.5" type="radio" className="rating" value="3.5" />
-        </label>
-        <label for="star3">
-          <input id="star3" type="radio" className="rating" value="3" />
-        </label>
-        <label for="star2.5">
-          <input id="star2.5" type="radio" className="rating" value="2.5" />
-        </label>
-        <label for="star2">
-          <input id="star2" type="radio" className="rating" value="2" />
-        </label>
-        <label for="star1.5">
-          <input id="star1.5" type="radio" className="rating" value="1.5" />
-        </label>
-        <label for="star1">
-          <input id="star1" type="radio" className="rating" value="1" />
-        </label>
-        <label for="star0.5">
-          <input id="star0.5" type="radio" className="rating" value="0.5" />
-        </label>
-        <label for="star0">
-          <input id="star0" type="radio" className="rating" value="0" />
-        </label>
-      </div>
+    <fieldset className="w-full h-full flex">
+      {[...Array(5)].map((star, index) => {
+        const currentRating = index + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={currentRating}
+              onClick={() => setRating(currentRating)}
+            />
+            <FaStar className="star" size={50} />
+          </label>
+        );
+      })}
     </fieldset>
   );
 };
